d7b0bc66c4694a8eee81a626ceb6a131f4aa8b00
yzhu <891085309@qq.com>
2022-09-19 06:15:44 +0800

    🐞 fix: add block_16

--- src/blockImage.js
+++ src/blockImage.js
@@ -1,18 +1,19 @@
-import block1 from "./assets/block/block1.png"
-import block2 from "./assets/block/block2.png"
-import block3 from "./assets/block/block3.png"
-import block4 from "./assets/block/block4.png"
-import block5 from "./assets/block/block5.png"
-import block6 from "./assets/block/block6.png"
-import block7 from "./assets/block/block7.png"
-import block8 from "./assets/block/block8.png"
-import block9 from "./assets/block/block9.png"
-import block10 from "./assets/block/block10.png"
-import block11 from "./assets/block/block11.png"
-import block12 from "./assets/block/block12.png"
-import block13 from "./assets/block/block13.png"
-import block14 from "./assets/block/block14.png"
-import block15 from "./assets/block/block15.png"
+import block1 from "./assets/block/block_1.png"
+import block2 from "./assets/block/block_2.png"
+import block3 from "./assets/block/block_3.png"
+import block4 from "./assets/block/block_4.png"
+import block5 from "./assets/block/block_5.png"
+import block6 from "./assets/block/block_6.png"
+import block7 from "./assets/block/block_7.png"
+import block8 from "./assets/block/block_8.png"
+import block9 from "./assets/block/block_9.png"
+import block10 from "./assets/block/block_10.png"
+import block11 from "./assets/block/block_11.png"
+import block12 from "./assets/block/block_12.png"
+import block13 from "./assets/block/block_13.png"
+import block14 from "./assets/block/block_14.png"
+import block15 from "./assets/block/block_15.png"
+import block16 from "./assets/block/block_16.png"
 
 const BlockImageMap = {
     1: block1,
@@ -30,6 +31,7 @@ const BlockImageMap = {
     13: block13,
     14: block14,
     15: block15,
+    16: block16,
 }
 
 export function GetBlockImageByType(type) {
