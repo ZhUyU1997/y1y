dfa8042348e4014f4882784826b15609a48c5f83
yzhu <891085309@qq.com>
2022-09-19 07:06:58 +0800

    ✨ feat: upload icon

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,30 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/y1y/',
-  plugins: [react(), VitePWA({})]
+  plugins: [react(), VitePWA({
+    registerType: "autoUpdate",
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+    },
+    manifest: {
+      name: '狼了个狼',
+      short_name: '狼了个狼',
+      description: '狼了个狼',
+      theme_color: "#ffffff",
+      background_color: "#ffffff",
+      display: "standalone",
+      icons: [
+        {
+          src: 'android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })]
 })
